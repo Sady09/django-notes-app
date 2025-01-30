4617f6516bcaca5f36e964f049d0edf2e14aab9f
Sady <joaosady667@gmail.com>
2025-01-29 22:44:54 -0300

    feat: adiciona endpoints da api

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Note(models.Model): 
+    id = models.UUIDField(max_length=30, primary_key=True, default=uuid.uuid4, editable=False) # o id é gerado automaticamete pelo UUID
+    note = models.TextField()
+    date = models.DateField(auto_now_add=True)
+
--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Note
+from rest_framework import viewsets
+from .serializer import NoteSerializer
 
 # Create your views here.
+def home(request):
+  return render(request, 'base.html')
+
+class NoteViewSet(viewsets.ModelViewSet):
+  queryset = Note.objects.all()
+  serializer_class = NoteSerializer
